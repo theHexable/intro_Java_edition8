f5480fff831a8b23b47026eaaa64626409b09d17
SBajary <sbajary@gmail.com>
2017-07-05 06:52:38 -0700

    chapter05, listings

---
+++ Chapter05/Listings/TestVoidMethod.java
@@ -0,0 +1,31 @@
+package Chapter05.Listings;
+
+/**
+ *
+ * @author Shady Bajary
+ * @Listing 5.2
+ */
+public class TestVoidMethod {
+
+    public static void main(String[] args) {
+        System.out.print("The grade is ");
+        printGrade(78.5);
+
+        System.out.print("The grade is ");
+        printGrade(59.5);
+    }
+
+    public static void printGrade(double score) {
+        if (score >= 90.0) {
+            System.out.println('A');
+        } else if (score >= 80.0) {
+            System.out.println('B');
+        } else if (score >= 70.0) {
+            System.out.println('C');
+        } else if (score >= 60) {
+            System.out.println('D');
+        } else {
+            System.out.println('F');
+        }
+    }
+}
