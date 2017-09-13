2f733323bc89009eda65cab1c0a12b82cd1be622
SBajary <sbajary@gmail.com>
2017-09-13 07:23:01 -0700

    chapter09, exercises

---
+++ Chapter09/Exercises/Exercise9_2.java
@@ -0,0 +1,26 @@
+package Chapter09.Exercises;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Shady Bajary
+ * @Exercise 9.2
+ * @Title Checking subString
+ */
+public class Exercise9_2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String str1 = scan.nextLine();
+        System.out.print("Enter the second string: ");
+        String str2 = scan.nextLine();
+        if (str1.indexOf(str2) >= 0) {
+            System.out.println("The string " + str2 + " found in " + str1);
+        }else{
+            System.out.println("The string " + str2 + " did not find in " + str1);
+        }
+
+    }
+}
---
+++ Chapter09/Exercises/Exercise9_3.java
@@ -0,0 +1,40 @@
+package Chapter09.Exercises;
+
+import java.util.Scanner;
+import sun.security.util.Password;
+
+/**
+ *
+ * @author Shady Bajary
+ * @Exercise 9.3
+ * @Title Checking Password
+ */
+public class Exercise9_3 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Password: ");
+        String password = scan.nextLine();
+        if (isValidPassword(password)) {
+            System.out.println(password + " is valid Password");
+        } else {
+            System.out.println(password + " is invalid Password");
+        }
+    }
+
+    public static boolean isValidPassword(String password) {
+        if (password.length() < 8) {
+            return false;
+        }
+        int countDigit = 0;
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))) {
+                countDigit++;
+            } else if (!Character.isAlphabetic(password.charAt(i))) {
+                return false;
+            }
+        }
+        return countDigit >= 2;
+
+    }
+}
